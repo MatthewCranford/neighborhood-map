0d30f2a8eafe208afa1728034cf09f9ca53d7c40
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-06 11:07:40 -0600

    Add Map component props

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -4,8 +4,23 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends Component {
 
   render() {
+    const style = {
+      width: '100%',
+      height: '100%'
+    }
+
+    const sunsetStrip= {
+      lat: 34.090698,
+      lng: -118.386002
+    }
+
     return (
-      <Map google={this.props.google}></Map>
+      <Map google={this.props.google} zoom={14} style={style} initialCenter={sunsetStrip}>
+        {/* <Marker onClick={this.onMarkerClick} name={'Current location'}/> */}
+        {/* <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>Test</div>
+        </InfoWindow> */}
+      </Map>
     )
   }
 }
