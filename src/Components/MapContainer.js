c07636ef6882e15175e0347ba8735856d1eb2156
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-07 10:56:54 -0600

    Add first Marker component

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -3,23 +3,36 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
 
+  state = {
+    places: [
+      { 
+        title: "Comedy Store",
+        location: {
+          lat: 34.095169, 
+          lng: -118.373918
+        }
+      }
+    ]
+    
+  }
+
   render() {
     const style = {
       width: '100%',
       height: '100%'
     }
 
-    const sunsetStrip= {
-      lat: 34.090698,
-      lng: -118.386002
-    }
+
 
     return (
-      <Map google={this.props.google} zoom={14} style={style} initialCenter={sunsetStrip}>
-        {/* <Marker onClick={this.onMarkerClick} name={'Current location'}/> */}
-        {/* <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>Test</div>
-        </InfoWindow> */}
+      <Map google={this.props.google} zoom={14} style={style} initialCenter={this.state.places[0].location}>
+        {this.state.places.map((place, index) => 
+          <Marker 
+            position={place.location}
+            title={place.title}
+            key={index}
+          />
+        )}
       </Map>
     )
   }
