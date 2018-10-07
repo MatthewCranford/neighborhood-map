a459c7a736a665ef10098d63614ec88c51acbc6e
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-07 13:43:46 -0600

    Add MapContainer state

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -4,6 +4,9 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends Component {
 
   state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
     places: [
       { 
         title: "Comedy Store",
@@ -14,25 +17,48 @@ export class MapContainer extends Component {
       }
     ]
     
-  }
+  };
+
+  // onMarkerClick = (props, marker, e) => {
+  //   console.log('Props: ',props)
+  //   this.setState({
+  //     selectedPlace: props,
+  //     activeMarker: marker,
+  //     showingInfoWindow: true
+  //   });
+  // }
 
   render() {
     const style = {
       width: '100%',
       height: '100%'
-    }
+    };
 
 
 
     return (
-      <Map google={this.props.google} zoom={14} style={style} initialCenter={this.state.places[0].location}>
+      <Map 
+        google={this.props.google} 
+        zoom={14} 
+        style={style} 
+        initialCenter={this.state.places[0].location}
+        
+      >
         {this.state.places.map((place, index) => 
           <Marker 
             position={place.location}
-            title={place.title}
+            name={'Current location'}
             key={index}
+            onClick={this.onMarkerClick}
           />
-        )}
+        )};
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
       </Map>
     )
   }
@@ -40,4 +66,4 @@ export class MapContainer extends Component {
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDohVkBpsX5m5lpmOXeS69UubK3rSaK2x8'
-})(MapContainer)
+})(MapContainer);
