4b2a060e34df1ce6bfbaa1ef104fb0e926625c22
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-09 09:36:40 -0600

    Add Map bounds

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -4,10 +4,23 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 class MapContainer extends Component {
 
   state = {
+    bounds: {},
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {}
   };
+
+  componentDidMount() {
+    this.setBounds();
+  }
+
+  setBounds = () => {
+    const bounds = new this.props.google.maps.LatLngBounds();
+    for (let place of this.props.places) {
+      bounds.extend(place.location);
+    }
+    this.setState({bounds});
+  }
 
   onMarkerClick = (props, marker) => {
     this.setState({
@@ -29,6 +42,7 @@ class MapContainer extends Component {
         zoom={14} 
         style={style} 
         initialCenter={this.props.places[0].location}
+        bounds={this.state.bounds}
       >
         {this.props.places.map((place, index) => 
           <Marker 
