f40121a5fbd0f1ed2f7a1d0aca3fa2b6dadae614
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-09 09:03:27 -0600

    Add more markers

--- src/App.js
+++ src/App.js
@@ -7,12 +7,33 @@ class App extends Component {
   state = {
     places: [
       { 
-        name: "Comedy Store",
+        name: "The Comedy Store",
         location: {
           lat: 34.095169, 
           lng: -118.373918
         }
-      }
+      },
+      { 
+        name: "Hollywood Improv Comedy Club",
+        location: {
+          lat: 34.083434, 
+          lng: -118.367281
+        }
+      },
+      { 
+        name: "Laugh Factory",
+        location: {
+          lat: 34.098247, 
+          lng: -118.364478 
+        }
+      },
+      { 
+        name: "The Groundlings Theatre & School",
+        location: {
+          lat: 34.083694, 
+          lng: -118.348995
+        }
+      },
     ]
   };
   
