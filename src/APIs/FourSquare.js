e1e8e9bbfd2de6906c53dc395d1b7977065f3601
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-27 08:52:53 -0600

    Add comments to FourSquare API functions

--- src/APIs/FourSquare.js
+++ src/APIs/FourSquare.js
@@ -1,9 +1,11 @@
+// Get venue ID
 export const getFourSquareVenueID = (lat, lng, name) => {
   return fetch(`https://api.foursquare.com/v2/venues/search?client_id=X4CMVBAJQSVZYXB45ZGE3GNA43RTCMPQTM4PUIKMQHFYWUVX&client_secret=ODC00AI1UEPGLLYLVUOY1JM30NE1XADBZRJMUNXKXPSZKNTR&v=20180323&limit=1&ll=${lat},${lng}&query=${name}`)
   .then((response) => response.json())
   .then((response) => response.response.venues[0].id);
 }
 
+// Get venue info data using the venue's ID
 export const getFourSquareVenueInfo = (venueId) => {
   return fetch(`https://api.foursquare.com/v2/venues/${venueId}?client_id=X4CMVBAJQSVZYXB45ZGE3GNA43RTCMPQTM4PUIKMQHFYWUVX&client_secret=ODC00AI1UEPGLLYLVUOY1JM30NE1XADBZRJMUNXKXPSZKNTR&v=20180323`)
   .then((response) => response.json())
