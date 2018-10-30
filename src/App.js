0f2e3f546d1961ad15d06e127ff2513f31c80751
Matthew Cranford <mcranfordmail@gmail.com>
2018-10-30 14:35:29 -0600

    Add FourSquare limit reached notification to user

--- src/App.js
+++ src/App.js
@@ -53,7 +53,8 @@ class App extends Component {
         likes: ''
       }
     ],
-    currentPlaces: []
+    currentPlaces: [],
+    requestAvailable: true
   };
 
   componentDidMount() {
@@ -71,9 +72,9 @@ class App extends Component {
               place.likes = venueInfo.likes.count
               place.img = venueInfo.bestPhoto.prefix + size + venueInfo.bestPhoto.suffix
             })
-            .catch((e) => console.log(e));
-        })
-        .catch((e) => console.log(e));
+            .catch(() => this.setState({ requestAvailable: false })
+        )})
+        .catch((e) => alert(e));
       return place;
     });
     this.setState({ currentPlaces: newPlaces });
@@ -97,7 +98,7 @@ class App extends Component {
     return (
       <div className="App">
         <MapNav places={this.state.currentPlaces} onQuery={this.filterPlaces} setActiveMarker={this.setActiveMarker}/>
-        <MapContainer places={this.state.currentPlaces} centerCoords={this.state.places[0].location} activeMarker={this.state.activeMarker} showingInfoWindow={this.state.showingInfoWindow}/>  
+        <MapContainer places={this.state.currentPlaces} centerCoords={this.state.places[0].location} activeMarker={this.state.activeMarker} showingInfoWindow={this.state.showingInfoWindow} requestAvailable={this.state.requestAvailable}/>  
       </div>
     );
   }
--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -41,6 +41,7 @@ class MapContainer extends Component {
       width: '100%',
       height: '100%'
     };
+    console.log(this.props.requestAvailable)
 
     return (
       <div>
@@ -64,12 +65,21 @@ class MapContainer extends Component {
           <InfoWindow
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}
+
           >
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-              <img src={this.state.selectedPlace.img} alt={this.state.selectedPlace.name}/>
-              <h3>Likes: {this.state.selectedPlace.likes}</h3>
-            </div>
+            { !this.props.requestAvailable ? (
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                <h3>Exceeded FourSquare API limit</h3>
+              </div>
+            ) : (
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                <img src={this.state.selectedPlace.img} alt={this.state.selectedPlace.name}/>
+                <h3>Likes: {this.state.selectedPlace.likes}</h3>
+              </div>
+            )
+          }
           </InfoWindow>
         </Map>
       </div>
